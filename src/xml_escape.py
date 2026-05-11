2abcae917a9f5f843e4010675272ed638e8bff74
Ivan13442 <averyanovivan24@gmail.com>
2026-05-11 18:20:26 +0400

    Add function to escape XML delimiters

---
+++ src/xml_escape.py
@@ -0,0 +1,8 @@
+def escape_xml_delimiters(text: str) -> str:
+    if not text:
+        return ""
+    return (
+        text.replace("&", "&amp;")
+        .replace("<", "&lt;")
+        .replace(">", "&gt;")
+    )
